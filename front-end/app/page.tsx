9841923d4cfb716a0335c0fec79b568930bdfa96
Simon Z. Ghebremeskel <81813787+simon-zerisenay@users.noreply.github.com>
2024-05-20 09:45:15 +0400

    Update page.tsx

--- front-end/app/page.tsx
+++ front-end/app/page.tsx
@@ -6,7 +6,7 @@ import Pricing from "@/components/section/pricing";
  
 
  
-export default function Home() {
+export default function Home() { 
 	return (
 		<section className="flex flex-col  items-center justify-center gap-4 py-8 md:py-10">
 			<Hero/>
