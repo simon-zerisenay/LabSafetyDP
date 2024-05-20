9e956e594ec68af599bec7c0bf0d5a04b6fb7808
Simon Z. Ghebremeskel <81813787+simon-zerisenay@users.noreply.github.com>
2024-05-20 09:53:36 +0400

    Update layout.tsx

--- front-end/app/layout.tsx
+++ front-end/app/layout.tsx
@@ -1,7 +1,7 @@
 import "@/styles/globals.css";
 import { Metadata, Viewport } from "next";
 import { siteConfig } from "@/config/site";
-import { fontSans } from "@/config/fonts";
+import { fontSans } from "@/config/fonts"; 
 import { Providers } from "./providers";
 import { Navbar } from "@/components/navbar";
 import { Link } from "@nextui-org/link";
